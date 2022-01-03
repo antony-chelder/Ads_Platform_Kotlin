913c2a00aa5bfbf56d47e606cabe0cd02694ace9
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 21:35:36 +0300

    Update AdPostFilter.kt

--- app/src/main/java/com/model/AdPostFilter.kt
+++ app/src/main/java/com/model/AdPostFilter.kt
@@ -1,10 +1,10 @@
 package com.model
 
-data class AdPostFilter( // Класс где будет фильтрация по времени и категориям
+data class AdPostFilter( 
     val time : String? = null,
     val cat_time : String? = null,
 
-    // Всевозможно фильтрация, чтобы брать с определенной категории
+    
     val cat_country_city_index_withsend_time : String? = null,
     val cat_country_index_withsend_time : String? = null,
     val cat_country_withsend_time : String? = null,
@@ -12,7 +12,7 @@ data class AdPostFilter( // Класс где будет фильтрация п
     val cat_index_withsend_time : String? = null,
 
 
-    // Всевозможно фильтрация, без определенной категории
+    
     val country_city_index_withsend_time : String? = null,
     val country_index_withsend_time : String? = null,
     val country_withsend_time : String? = null,
