c367e5afb8313bb40e69e0a9f7560e55442a7fb2
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 21:42:12 +0300

    Update BillingManager.kt

--- app/src/main/java/com/utils/BillingManager.kt
+++ app/src/main/java/com/utils/BillingManager.kt
@@ -4,19 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.billingclient.api.*
 
 class BillingManager(val act : AppCompatActivity) {
-    private var billingClient : BillingClient? = null // Инстанция BillingClient через который будут реализововатся покупки
+    private var billingClient : BillingClient? = null 
 
 
     init {
      setAppBillingClient()
     }
 
-    private fun setAppBillingClient(){ // Функция для настройки BillingClient
-        billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build() // Настройка BillingClient
+    private fun setAppBillingClient(){ 
+        billingClient = BillingClient.newBuilder(act).setListener(getPurchaseListener()).enablePendingPurchases().build() 
     }
 
 
-     fun startConnection(){ // Функция которая запускается когда совершается покупка
+     fun startConnection(){ 
          billingClient?.startConnection(object : BillingClientStateListener{
              override fun onBillingServiceDisconnected() {
 
@@ -30,33 +30,32 @@ class BillingManager(val act : AppCompatActivity) {
     }
 
 
-    private fun getItem(){ // Создаем продукт и запускаем диалог с Плеймаркете, с информацией о покупке
-        val pokupList = ArrayList<String>() // Создача списка покупок, хоть и одно, но все равно создается как массив
-        pokupList.add(REMOVE_ADS) // Добавляем все покупки которые есть
-        val pokupDetails = SkuDetailsParams.newBuilder() // Детали покупки,информация о покупки
-        pokupDetails.setSkusList(pokupList).setType(BillingClient.SkuType.INAPP) // Сформировали полную информацию о покупке, указали тип встроенные покупки
-        billingClient?.querySkuDetailsAsync(pokupDetails.build()){ // Создали запрос
+    private fun getItem(){ 
+        val pokupList = ArrayList<String>() 
+        pokupList.add(REMOVE_ADS) 
+        val pokupDetails = SkuDetailsParams.newBuilder() 
+        pokupDetails.setSkusList(pokupList).setType(BillingClient.SkuType.INAPP) 
+        billingClient?.querySkuDetailsAsync(pokupDetails.build()){ 
                 result,list ->
             run {
-                if (result.responseCode == BillingClient.BillingResponseCode.OK) { // Проверка, если все прошло успешно
-                    if(!list.isNullOrEmpty()){ // Проверка если все прошло успешно, то создается Launcher( диалог с покупкой)
-                      val billingFlowParams = BillingFlowParams.newBuilder().setSkuDetails(list[0]).build() // Подготовка, что есть такая покупка на плеймаркете и все готово
-                        billingClient?.launchBillingFlow(act,billingFlowParams) // Запуск launcher(диалога) с покупкой
+                if (result.responseCode == BillingClient.BillingResponseCode.OK) { 
+                    if(!list.isNullOrEmpty()){ 
+                      val billingFlowParams = BillingFlowParams.newBuilder().setSkuDetails(list[0]).build() 
+                        billingClient?.launchBillingFlow(act,billingFlowParams) 
                     }
                 }
             }
 
-        }// Отправка данные асинхронно, не останавливая основной поток
-
+        }
 
     }
 
-    private fun getPurchaseListener() : PurchasesUpdatedListener{ // Listener который прослушивает, что происходит с покупками
+    private fun getPurchaseListener() : PurchasesUpdatedListener{ 
      return PurchasesUpdatedListener{
          result,list ->
          run {
-             if (result.responseCode == BillingClient.BillingResponseCode.OK) { // Проверка, если все прошло успешно
-              list?.get(0).let { // Берем с 0 позиции так как будет 1 покупка
+             if (result.responseCode == BillingClient.BillingResponseCode.OK) { 
+              list?.get(0).let { 
 
               }
              }
