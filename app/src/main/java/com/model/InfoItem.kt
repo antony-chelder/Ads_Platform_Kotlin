5382211a801a952407e943c308ee89ceb2644bb9
Antony Chepel <71262438+antony-chelder@users.noreply.github.com>
2022-01-03 21:41:11 +0300

    Update InfoItem.kt

--- app/src/main/java/com/model/InfoItem.kt
+++ app/src/main/java/com/model/InfoItem.kt
@@ -1,7 +1,7 @@
 package com.model
 
-data class InfoItem( // Класс который будет хранить в себе информацию о количестве просмотров, сообщений, звонков
-    val  viewsCounter : String? = null, // Указывем что по умолчанию значение может быть null
-    val  emailsCounter : String? = null, // Указывем что по умолчанию значение может быть null
-    val  callsCounter : String? = null // Указывем что по умолчанию значение может быть null
+data class InfoItem( 
+    val  viewsCounter : String? = null, 
+    val  emailsCounter : String? = null, 
+    val  callsCounter : String? = null 
 )
